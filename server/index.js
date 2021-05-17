f46b920dc5e1213f9d01cb3b218fe9bd371a3b83
Jahnavi Kedia <jahnavikedia91@gmail.com>
2021-05-18 01:09:28 +0530

    added base setup for server

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+
+const app = express();
+
+app.use(express.json({ limit: "30mb", extended: true}));
+app.use(express.urlencoded({ limit: "30mb", extended: true}));
+
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONNECTION_URL,{useNewUrlParser: true, useUnifiedTopology: true})
+    .then(()=>app.listen(PORT,()=> console.log(`Server running at: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+    mongoose.set('useFindAndModify',false);
